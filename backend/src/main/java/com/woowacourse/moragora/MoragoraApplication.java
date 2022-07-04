810757f84144315c5ffb3ef897d72bb8c5899dde
sun <67397679+syoun602@users.noreply.github.com>
2022-07-04 19:19:24 +0900

    [BE] 프로젝트 환경설정

---
+++ backend/src/main/java/com/woowacourse/moragora/MoragoraApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.moragora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoragoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoragoraApplication.class, args);
+	}
+
+}
