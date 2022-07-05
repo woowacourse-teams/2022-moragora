2b9a854f8befb10ccded01df1d0b68189de465fc
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-05 11:51:58 +0900

    fix: eslint 이슈 수정
    - 파일 확장자 강제 규칙 해제
    - require를 top level에서만
    사용하도록 강제하는 규칙 해제
    - 모든 확장자에서 jsx 문법 허용
    - babel emotion 플러그인 적용
    - react import 강제 규칙 해제
    - spreading 문법 금지 규칙 해제
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,10 +7,11 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
     'plugin:prettier/recommended',
-    '@typescript-eslint',
     'plugin:storybook/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -19,7 +20,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/function-component-definition': [
       2,
@@ -27,5 +28,27 @@ module.exports = {
         namedComponents: 'arrow-function',
       },
     ],
+    // 파일 확장자 강제하지 않도록 함
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    // 조건문에서 require 사용 가능하도록 함
+    'global-require': 0,
+    // 모든 확장자에서 jsx 사용 가능하도록 함
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    // react import 강제하지 않도록 함
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
