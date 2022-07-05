1c8f1b12a320370b3e457bb9bdf3c96f938cbe1a
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-05 14:48:32 +0900

    [FE] 리액트 앱을 만들기 위한 환경 설정 (#16)
    * chore: package.json 초기화
    
    * chore: eslint, prettier 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: 리액트앱 툴체인 설정
    
    - babel
    - webpack
    - typescript
    - react
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: storybook 설정
    
    * chore: msw 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: emotion 설정
    
    - typescript 설정
    - storybook을 위한 babel 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * fix: eslint 이슈 수정
    
    - 파일 확장자 강제 규칙 해제
    - require를 top level에서만
    사용하도록 강제하는 규칙 해제
    - 모든 확장자에서 jsx 문법 허용
    - babel emotion 플러그인 적용
    - react import 강제 규칙 해제
    - spreading 문법 금지 규칙 해제
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * style: react import 제거
    
    * style: eol 추가
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    // 파일 확장자 강제하지 않도록 함
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    // 조건문에서 require 사용 가능하도록 함
+    'global-require': 0,
+    // 모든 확장자에서 jsx 사용 가능하도록 함
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    // react import 강제하지 않도록 함
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
