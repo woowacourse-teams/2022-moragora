e6c89262a800526d7b93042e072806effbccd260
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-05 14:48:32 +0900

    [FE] 리액트 앱을 만들기 위한 환경 설정 (#16)
    * chore: package.json 초기화
    
    * chore: eslint, prettier 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: 리액트앱 툴체인 설정
    
    - babel
    - webpack
    - typescript
    - react
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: storybook 설정
    
    * chore: msw 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * chore: emotion 설정
    
    - typescript 설정
    - storybook을 위한 babel 설정
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * fix: eslint 이슈 수정
    
    - 파일 확장자 강제 규칙 해제
    - require를 top level에서만
    사용하도록 강제하는 규칙 해제
    - 모든 확장자에서 jsx 문법 허용
    - babel emotion 플러그인 적용
    - react import 강제 규칙 해제
    - spreading 문법 금지 규칙 해제
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>
    
    * style: react import 제거
    
    * style: eol 추가
    
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build',
+    filename: '[name].bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 8000,
+    open: true,
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Moragora',
+    }),
+    new ESLintPlugin(),
+  ],
+};
