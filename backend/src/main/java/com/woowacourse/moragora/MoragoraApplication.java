90b4ca930552bfa328b5ac0e56ac6061e96236e8
Hongdonggeon <ghd700@naver.com>
2022-07-06 15:54:33 +0900

    style: 모라고라 코드 컨벤션 적용

--- backend/src/main/java/com/woowacourse/moragora/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/moragora/MoragoraApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoragoraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoragoraApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoragoraApplication.class, args);
+    }
 }
