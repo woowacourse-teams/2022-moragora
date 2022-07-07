bad0c73f54efdf5eab7a9b0bea1df43e7a118a8d
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-07 12:50:01 +0900

    feat: Opinion 타입 정의
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

--- frontend/src/types/OpinionTypes.ts
+++ frontend/src/types/OpinionTypes.ts
@@ -1,6 +1,11 @@
 export type Opinion = {
   id: number;
+  discussionId: number;
   content: string;
   createdAt: number;
   updatedAt: number | null;
+};
+
+export type OpinionRequestBody = {
+  content: Opinion['content'];
 };
