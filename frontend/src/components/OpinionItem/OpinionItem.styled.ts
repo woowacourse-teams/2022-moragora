fda570db6028d7c15339e765ae25d17b56b9fc7c
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-07 11:50:40 +0900

    feat: OpinonItem, Opinion section 컴포넌트 및 ui 구현
    Co-authored-by: kamwoo <kamwoo@users.noreply.github.com>

---
+++ frontend/src/components/OpinionItem/OpinionItem.styled.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 0.75rem;
+`;
+
+export const nameSpan = styled.span`
+  font-size: 1rem;
+`;
+
+export const Box = styled.div`
+  display: flex;
+`;
+
+export const TimestampParagraph = styled.p`
+  margin: 0;
+  margin-left: 1rem;
+`;
+
+export const OpinionParagraph = styled.p`
+  margin: 0;
+  display: block;
+  word-break: break-all;
+`;
+
+export const UpdatedSpan = styled.span`
+  margin-left: 1rem;
+  color: gray;
+`;
