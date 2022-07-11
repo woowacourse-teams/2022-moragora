f8cf48a5e07c6567af2d2953ab1c2d1fb1d0c408
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-11 17:18:24 +0900

    feat: MeetingCreatePage 어트리뷰트 설정

--- frontend/src/pages/MeetingCreatePage/index.tsx
+++ frontend/src/pages/MeetingCreatePage/index.tsx
@@ -11,17 +11,28 @@ const MeetingCreatePage = () => {
         <S.TitleSection>
           <h2>새로운 모임 만들기</h2>
         </S.TitleSection>
-        <S.Form>
+        <S.Form
+          id="meeting-create-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            const target = e.target as HTMLFormElement;
+            const formData = new FormData(target);
+            const formDataObject = Object.fromEntries(formData.entries());
+
+            console.log(formDataObject);
+          }}
+        >
           <S.FieldBox>
             <S.Label>
               모임명
-              <Input type="text" />
+              <Input type="text" name="title" required />
             </S.Label>
           </S.FieldBox>
           <S.FieldBox>
             <S.Label>
               팀원
-              <Input type="text" />
+              <Input type="text" name="member" />
             </S.Label>
           </S.FieldBox>
           <S.FieldBox>
@@ -29,6 +40,7 @@ const MeetingCreatePage = () => {
               시작 날짜
               <Input
                 type="date"
+                name="start-date"
                 onClick={(e: React.MouseEvent<HTMLInputElement>) => {
                   const target = e.target as HTMLInputElement & {
                     showPicker: () => void;
@@ -36,12 +48,14 @@ const MeetingCreatePage = () => {
 
                   target.showPicker();
                 }}
+                required
               />
             </S.Label>
             <S.Label>
               마감 날짜
               <Input
                 type="date"
+                name="end-date"
                 onClick={(e: React.MouseEvent<HTMLInputElement>) => {
                   const target = e.target as HTMLInputElement & {
                     showPicker: () => void;
@@ -49,6 +63,7 @@ const MeetingCreatePage = () => {
 
                   target.showPicker();
                 }}
+                required
               />
             </S.Label>
           </S.FieldBox>
@@ -56,7 +71,8 @@ const MeetingCreatePage = () => {
             <S.Label>
               시작 시간
               <Input
-                type="date"
+                type="time"
+                name="start-time"
                 onClick={(e: React.MouseEvent<HTMLInputElement>) => {
                   const target = e.target as HTMLInputElement & {
                     showPicker: () => void;
@@ -64,12 +80,14 @@ const MeetingCreatePage = () => {
 
                   target.showPicker();
                 }}
+                required
               />
             </S.Label>
             <S.Label>
               마감 시간
               <Input
-                type="date"
+                type="time"
+                name="end-time"
                 onClick={(e: React.MouseEvent<HTMLInputElement>) => {
                   const target = e.target as HTMLInputElement & {
                     showPicker: () => void;
@@ -77,13 +95,14 @@ const MeetingCreatePage = () => {
 
                   target.showPicker();
                 }}
+                required
               />
             </S.Label>
           </S.FieldBox>
         </S.Form>
       </S.Layout>
       <Footer>
-        <Button form="attendance-form" type="submit">
+        <Button form="meeting-create-form" type="submit">
           추가하기
         </Button>
       </Footer>
