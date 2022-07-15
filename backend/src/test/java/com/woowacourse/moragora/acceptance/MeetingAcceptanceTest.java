06566ca4e0c0840070446295f044e6789c080a8f
syoun602 <syoun602@gmail.com>
2022-07-15 18:07:24 +0900

    PR 열렸을 때 CICD 동작하는지 확인

--- backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
@@ -24,6 +24,7 @@ public class MeetingAcceptanceTest extends AcceptanceTest {
     @Test
     void add() {
         // given
+        // 라벨 테스팅
         final MeetingRequest meetingRequest = new MeetingRequest(
                 "모임1",
                 LocalDate.of(2022, 7, 10),
