14c20ef8360dd2fda7f6f64dfc31d0f81878886f
kamwoo <rkadndud06@naver.com>
2022-07-15 11:32:03 +0900

    feat: router 컴포넌트 구현
    Co-authored-by: Jeongmin Woo <greenblues1190@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import MobileLayout from './components/layouts/MobileLayout';
 import Header from './components/layouts/Header';
-import MeetingPage from './pages/MeetingPage';
 import { css, ThemeProvider } from '@emotion/react';
 import { theme } from './styles/themes/theme';
+import { BrowserRouter } from 'react-router-dom';
+import Router from 'router';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <div
@@ -22,12 +23,12 @@ const App = () => {
         >
           <MobileLayout>
             <Header />
-            <MeetingPage />
+            <Router />
           </MobileLayout>
         </div>
         <div id="root-modal" />
       </ThemeProvider>
-    </>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,25 @@
+import MeetingCreatePage from 'pages/MeetingCreatePage';
+import MeetingListPage from 'pages/MeetingListPage';
+import MeetingPage from 'pages/MeetingPage';
+import RegisterPage from 'pages/RegisterPage';
+import { Navigate, useRoutes } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+
+const Router = () => {
+  return useRoutes([
+    { path: '/', element: <Navigate to="/login" replace /> },
+    { path: '/login', element: <LoginPage /> },
+    { path: '/register', element: <RegisterPage /> },
+    {
+      path: '/meeting',
+      children: [
+        { path: '', element: <MeetingListPage /> },
+        { path: ':id', element: <MeetingPage /> },
+        { path: 'create', element: <MeetingCreatePage /> },
+      ],
+    },
+    { path: '*', element: <div>에러페이지</div> },
+  ]);
+};
+
+export default Router;
