d54d58ec3d201a1761e010bdb3756948a273a446
progress0407 <progress0407@gmail.com>
2022-07-15 15:36:05 +0900

    add line

--- backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
@@ -26,6 +26,7 @@ public class MeetingAcceptanceTest extends AcceptanceTest {
     void add() {
         // given
 
+
         final MeetingRequest meetingRequest = new MeetingRequest(
                 "모임1",
                 LocalDate.of(2022, 7, 10),
