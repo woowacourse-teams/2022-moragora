7db7cfae0f29b7385a7ce3e686658e1f1de42454
syoun602 <syoun602@gmail.com>
2022-07-15 17:46:04 +0900

    PR 열렸을 때 CICD 동작하는지 확인

--- backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
@@ -36,6 +36,8 @@ public class MeetingAcceptanceTest extends AcceptanceTest {
         // when
         final ValidatableResponse response = post("/meetings", meetingRequest);
 
+
+
         // then
         response.statusCode(HttpStatus.CREATED.value())
                 .header("Location", notNullValue());
