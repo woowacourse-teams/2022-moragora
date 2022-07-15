fcc0a4e5e6fe6f1965ffb9cd60e1eac5112f31d2
progress0407 <progress0407@gmail.com>
2022-07-15 17:50:37 +0900

    test jenkins

--- backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/moragora/acceptance/MeetingAcceptanceTest.java
@@ -24,6 +24,8 @@ public class MeetingAcceptanceTest extends AcceptanceTest {
     @Test
     void add() {
         // given
+
+        // test용
         final MeetingRequest meetingRequest = new MeetingRequest(
                 "모임1",
                 LocalDate.of(2022, 7, 10),
