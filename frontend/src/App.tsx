7274fcd55b82c83124fbe6a8c2db00439b5966d5
kamwoo <rkadndud06@naver.com>
2022-07-15 14:12:56 +0900

    feat: BrowserRouter, GlobalStyles, ThemProvider 이동
    - 기존에 App파일에서 index 파일로 위치 변경
    
    Co-authored-by: Jeongmin Woo <greenblues1190@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,28 @@
 import React from 'react';
-import GlobalStyles from './styles/GlobalStyles';
 import MobileLayout from './components/layouts/MobileLayout';
 import Header from './components/layouts/Header';
-import { css, ThemeProvider } from '@emotion/react';
-import { theme } from './styles/themes/theme';
-import { BrowserRouter } from 'react-router-dom';
-import Router from 'router';
+import { css } from '@emotion/react';
+import Router from './router';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <div
-          css={css`
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          `}
-        >
-          <MobileLayout>
-            <Header />
-            <Router />
-          </MobileLayout>
-        </div>
-        <div id="root-modal" />
-      </ThemeProvider>
-    </BrowserRouter>
+    <>
+      <div
+        css={css`
+          width: 100vw;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <MobileLayout>
+          <Header />
+          <Router />
+        </MobileLayout>
+      </div>
+      <div id="root-modal" />
+    </>
   );
 };
 
