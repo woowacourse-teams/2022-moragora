c117e046ba6b4ec07e83163dc3d6ef4b9607f0e5
kamwoo <rkadndud06@naver.com>
2022-07-16 18:20:24 +0900

    feat: CoffeeItem 스토리북 args 수정

--- frontend/src/components/CoffeeStackItem/CoffeeStackItem.stories.jsx
+++ frontend/src/components/CoffeeStackItem/CoffeeStackItem.stories.jsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import CoffeeStackItem from '.';
 
 export default {
@@ -11,13 +12,5 @@ const Template = (args) => {
 
 export const Default = Template.bind({});
 Default.args = {
-  meeting: {
-    id: 1,
-    name: '모임1',
-    attendanceCount: 0,
-    startDate: '2022-01-01',
-    endDate: '2022-12-31',
-    entranceTime: '10:00',
-    leaveTime: '18:00',
-  },
+  name: '모임 1',
 };
