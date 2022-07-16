7e4b9555c306bc92f19da828577e207c1aa1419c
kamwoo <rkadndud06@naver.com>
2022-07-16 16:29:45 +0900

    feat: 미팅 시작 시간 추가

--- frontend/src/components/MeetingItem/index.tsx
+++ frontend/src/components/MeetingItem/index.tsx
@@ -2,6 +2,32 @@ import * as S from './MeetingItem.styled';
 import MeetingIconSVG from '../../assets/meeting.svg';
 import React from 'react';
 
+// minute이 60을 넘어갈 때 hour + 1
+// 24시를 넘어갈 때 0으로 수정
+function reconcileTime(time: string) {
+  const timeList = time.split(':');
+  let hour = parseInt(timeList[0]);
+  let minute = parseInt(timeList[1]);
+
+  if (minute >= 60) {
+    hour += 1;
+    minute -= 60;
+  }
+
+  return `${('0' + (hour % 24)).slice(-2)}:${('0' + minute).slice(-2)}`;
+}
+
+// 분에 minute에 해당하는만큼 추가
+function addMinute(startTime: string, minute: number) {
+  const timeList = startTime.split(':');
+  let startHour = timeList[0];
+  let startMinute = parseInt(timeList[1]);
+
+  const addedTime = `${startHour}:${('0' + (startMinute + minute)).slice(-2)}`;
+
+  return reconcileTime(addedTime);
+}
+
 type Meeting = {
   id: number;
   name: string;
@@ -21,7 +47,9 @@ const MeetingItem: React.FC<MeetingItemProps> = ({ meeting }) => {
     <S.Layout>
       <S.MeetingIconImage src={MeetingIconSVG} />
       <span>{meeting.name}</span>
-      <span>10:00 ~ 10:05</span>
+      <span>
+        {meeting.entranceTime} ~ {addMinute(meeting.entranceTime, 5)}
+      </span>
       <S.CheckInSpan>체크인하세요!</S.CheckInSpan>
     </S.Layout>
   );
