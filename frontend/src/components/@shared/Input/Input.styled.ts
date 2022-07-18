c681baa07b1586276f3235d0bf4b4d00655e5e32
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-18 17:35:46 +0900

    fix: Input 컴포넌트 width 설정

--- frontend/src/components/@shared/Input/Input.styled.ts
+++ frontend/src/components/@shared/Input/Input.styled.ts
@@ -4,6 +4,7 @@ export const Input = styled.input`
   padding: 0.75rem;
   border: 1px solid ${({ theme: { colors } }) => colors['subtle-light']};
   border-radius: 0.5rem;
+  width: 100%;
 
   :focus {
     outline-color: 2px solid ${({ theme: { colors } }) => colors['primary']};
