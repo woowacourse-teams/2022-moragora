1fdc7554a5014349d234f25bb1173962d631a677
kamwoo <rkadndud06@naver.com>
2022-07-19 01:25:12 +0900

    refactor: 기능 변경으로 인한 쓰지않는 로직 제거

--- frontend/src/pages/MeetingPage/index.tsx
+++ frontend/src/pages/MeetingPage/index.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import * as S from './MeetingPage.styled';
 import Footer from '../../components/layouts/Footer';
-import Button from '../../components/@shared/Button';
 import useFetch from '../../hooks/useFetch';
 import Spinner from '../../components/@shared/Spinner';
 import ErrorIcon from '../../components/@shared/ErrorIcon';
-import ModalPortal from '../../components/ModalPortal';
-import ModalWindow from '../../components/@shared/ModalWindow';
 import DivideLine from '../../components/@shared/DivideLine';
 import ReloadButton from '../../components/@shared/ReloadButton';
-import useForm from '../../hooks/useForm';
 import { useParams } from 'react-router-dom';
 import UserItem from '../../components/UserItem';
 
@@ -34,26 +30,6 @@ type MeetingResponseBody = {
   attendanceCount: number;
 };
 
-type FormDataObject = { [k: string]: FormDataEntryValue };
-
-type MeetingStateDataType = {
-  data: MeetingResponseBody;
-  loading: boolean;
-  error: any;
-  fetchingCount: number;
-  refetch: () => void;
-};
-
-const submitAttendanceData = async (url: string, payload: any) => {
-  return fetch(url, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  });
-};
-
 const userAttendanceFetch = async (url: string, payload: any) => {
   return fetch(url, {
     method: 'PUT',
@@ -67,61 +43,16 @@ const userAttendanceFetch = async (url: string, payload: any) => {
 const MeetingPage = () => {
   const { id } = useParams();
   const meetingState = useFetch<MeetingResponseBody>(`/meetings/${id}`);
-  const [modalOpened, setModalOpened] = useState(false);
-  const [formData, setFormData] = useState<FormDataObject>();
-  const { isSubmitting, onSubmit, register } = useForm();
 
   const handleChecked = async (
     user: Omit<User, 'accessToken'>,
     checked: boolean
   ) => {
-    const targetMeeting = meetingState.data.users.find(
-      (userData) => userData.id === user.id
-    );
-    targetMeeting.attendanceStatus = checked ? 'present' : 'tardy';
-
-    const response = await userAttendanceFetch(
-      `/meetings/${id}/users/${user.id}`,
-      {
-        attendanceStatus: checked ? 'present' : 'tardy',
-      }
-    );
+    await userAttendanceFetch(`/meetings/${id}/users/${user.id}`, {
+      attendanceStatus: checked ? 'present' : 'tardy',
+    });
 
     meetingState.refetch();
-  };
-
-  const handleOpen = () => {
-    setModalOpened(true);
-  };
-
-  const handleClose = () => {
-    setModalOpened(false);
-  };
-
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    const target = e.target as HTMLFormElement;
-    const formData = new FormData(target);
-    const formDataObject = Object.fromEntries(formData.entries());
-
-    setFormData(formDataObject);
-
-    handleOpen();
-  };
-
-  const handleConfirm = async () => {
-    const payload = Object.entries(formData).map(([id, value]) => ({
-      id,
-      isAbsent: value === 'absent',
-    }));
-
-    const response = await submitAttendanceData('/meetings/1', payload);
-
-    if (response.ok) {
-      alert('출결 마감했습니다.');
-      handleClose();
-
-      meetingState.refetch();
-    }
   };
 
   if (meetingState.loading) {
@@ -132,11 +63,7 @@ const MeetingPage = () => {
             <Spinner />
           </S.SpinnerBox>
         </S.Layout>
-        <Footer>
-          <Button form="attendance-form" type="submit" disabled>
-            출결 마감
-          </Button>
-        </Footer>
+        <Footer />
       </>
     );
   }
@@ -154,26 +81,13 @@ const MeetingPage = () => {
             />
           </S.ErrorBox>
         </S.Layout>
-        <Footer>
-          <Button form="attendance-form" type="submit" disabled>
-            출결 마감
-          </Button>
-        </Footer>
+        <Footer />
       </>
     );
   }
 
   return (
     <>
-      {modalOpened && (
-        <ModalPortal closePortal={handleClose}>
-          <ModalWindow
-            message="마감하시겠습니까?"
-            onConfirm={handleConfirm}
-            onDismiss={handleClose}
-          />
-        </ModalPortal>
-      )}
       <S.Layout>
         <S.MeetingDetailSection>
           <h2>{meetingState.data.name}</h2>
@@ -183,7 +97,7 @@ const MeetingPage = () => {
         </S.MeetingDetailSection>
         <DivideLine />
         <S.UserListSection>
-          <S.Form id="attendance-form" {...onSubmit(handleSubmit)}>
+          <S.UserListBox>
             <S.UserList>
               {meetingState.data.users.map((user) => (
                 <UserItem
@@ -193,7 +107,7 @@ const MeetingPage = () => {
                 />
               ))}
             </S.UserList>
-          </S.Form>
+          </S.UserListBox>
         </S.UserListSection>
       </S.Layout>
       <Footer />
