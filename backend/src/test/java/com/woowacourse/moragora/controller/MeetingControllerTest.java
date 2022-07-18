f4ada559e5c87e805fa20a582796552ae678f82a
YJGwon <yj970125@gmail.com>
2022-07-18 19:32:06 +0900

    test: todo 주석 추가

--- backend/src/test/java/com/woowacourse/moragora/controller/MeetingControllerTest.java
+++ backend/src/test/java/com/woowacourse/moragora/controller/MeetingControllerTest.java
@@ -36,6 +36,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+// TODO: LoginArgumentResolver return mocking 시도
 @WebMvcTest(controllers = {MeetingController.class})
 @MockBean(value = {WebConfig.class})
 class MeetingControllerTest {
