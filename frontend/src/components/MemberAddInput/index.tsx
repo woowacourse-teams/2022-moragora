61c0fc1b7b6d45347228e36f686f9810c9275261
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-18 21:09:43 +0900

    fix: path 프로퍼티명 수정

--- frontend/src/components/MemberAddInput/index.tsx
+++ frontend/src/components/MemberAddInput/index.tsx
@@ -21,7 +21,7 @@ const MemberAddInput: React.FC<React.HTMLAttributes<HTMLInputElement>> = (
     unselectItem,
     clearQueryResult,
   } = useQuerySelectItems<User>('/users?keyword=', {
-    delay: 200,
+    wait: 150,
   });
   const [value, setValue] = useState('');
   const [isShowingPopup, setIsShowingPopup] = useState(false);
@@ -103,8 +103,8 @@ const MemberAddInput: React.FC<React.HTMLAttributes<HTMLInputElement>> = (
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M8 16C10.1217 16 12.1566 15.1571 13.6569 13.6569C15.1571 12.1566 16 10.1217 16 8C16 5.87827 15.1571 3.84344 13.6569 2.34315C12.1566 0.842855 10.1217 0 8 0C5.87827 0 3.84344 0.842855 2.34315 2.34315C0.842855 3.84344 0 5.87827 0 8C0 10.1217 0.842855 12.1566 2.34315 13.6569C3.84344 15.1571 5.87827 16 8 16V16ZM5 7C4.73478 7 4.48043 7.10536 4.29289 7.29289C4.10536 7.48043 4 7.73478 4 8C4 8.26522 4.10536 8.51957 4.29289 8.70711C4.48043 8.89464 4.73478 9 5 9H11C11.2652 9 11.5196 8.89464 11.7071 8.70711C11.8946 8.51957 12 8.26522 12 8C12 7.73478 11.8946 7.48043 11.7071 7.29289C11.5196 7.10536 11.2652 7 11 7H5Z"
                 />
               </svg>
