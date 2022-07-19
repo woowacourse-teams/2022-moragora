09dc0d74404a40085900e7b4ad7c9d035aff3350
sun <67397679+syoun602@users.noreply.github.com>
2022-07-19 20:43:13 +0900

    [BE] Interceptor 및 ArgumentResolver 등록 방식 변경 (#137)
    * refactor: Interceptor 및 ArgumentResolver 등록 방식 변경
    
    * [BE] keyword를 포함한 User정보를 찾는 기능 구현 및 테스트 (#124)
    
    * feat: keyword를 통한 User 검색 기능 구현
    
    * feat: keyword가 입력되지 않았을 경우 예외 핸들링
    
    * refactor: 메서드명 적절히 변경
    
    * refactor: Interceptor 및 ArgumentResolver 등록 방식 변경
    
    Co-authored-by: Dongseok Shin <70707629+shindong96@users.noreply.github.com>

--- backend/src/main/java/com/woowacourse/auth/config/WebConfig.java
+++ backend/src/main/java/com/woowacourse/auth/config/WebConfig.java
@@ -1,41 +1,33 @@
 package com.woowacourse.auth.config;
 
-import com.woowacourse.auth.support.JwtTokenProvider;
 import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final JwtTokenProvider jwtTokenProvider;
+    private final List<HandlerInterceptor> interceptors;
+    private final List<HandlerMethodArgumentResolver> resolvers;
 
-    public WebConfig(final JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
+    public WebConfig(final List<HandlerInterceptor> interceptors,
+                     final List<HandlerMethodArgumentResolver> resolvers) {
+        this.interceptors = interceptors;
+        this.resolvers = resolvers;
     }
+
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        final LoginInterceptor interceptor = loginInterceptor();
-        registry.addInterceptor(interceptor)
-                .addPathPatterns("/**");
+        interceptors.forEach(interceptor -> registry.addInterceptor(interceptor)
+                .addPathPatterns("/**"));
     }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(loginArgumentResolver());
-    }
-
-    @Bean
-    public LoginInterceptor loginInterceptor() {
-        return new LoginInterceptor(jwtTokenProvider);
-    }
-
-    @Bean
-    public LoginArgumentResolver loginArgumentResolver() {
-        return new LoginArgumentResolver(jwtTokenProvider);
+        resolvers.addAll(this.resolvers);
     }
 }
