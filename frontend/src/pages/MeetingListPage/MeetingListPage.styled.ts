6bbde350edfce6a8e40c6d7d1fdc7741d53f62ca
Jeongmin Woo <greenblues1190@gmail.com>
2022-07-21 02:00:22 +0900

    feat: 참여 모임 Empty state ui 추가

--- frontend/src/pages/MeetingListPage/MeetingListPage.styled.ts
+++ frontend/src/pages/MeetingListPage/MeetingListPage.styled.ts
@@ -97,3 +97,53 @@ export const CoffeeStackList = styled.ul`
   overflow-x: hidden;
   overflow-y: scroll;
 `;
+
+export const EmptyStateBox = styled.div`
+  flex: 1;
+  padding: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+export const MeetingCreateLink = styled(Link)`
+  // reset
+  color: inherit;
+  text-decoration: inherit;
+
+  text-align: center;
+  width: 100%;
+  background-color: ${({ theme: { colors } }) => colors['primary']};
+  color: ${({ theme: { colors } }) => colors['white']};
+  padding: 0.75rem 0;
+  border-radius: 1.5rem;
+
+  :hover {
+    background-color: ${({ theme: { colors } }) => colors['primary-subtle']};
+  }
+
+  :disabled {
+    background-color: ${({ theme: { colors } }) => colors['background']};
+    color: ${({ theme: { colors } }) => colors['subtle-light']};
+    pointer-events: none;
+  }
+`;
+
+export const EmptyStateImage = styled.img`
+  margin-top: -2rem;
+  padding-bottom: 2rem;
+  width: 100%;
+`;
+
+export const EmptyStateTitle = styled.h2`
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: ${({ theme: { colors } }) => colors['subtle-dark']};
+`;
+
+export const EmptyStateParagraph = styled.p`
+  font-size: 1rem;
+  color: ${({ theme: { colors } }) => colors['subtle-light']};
+`;
--- frontend/src/pages/MeetingListPage/index.tsx
+++ frontend/src/pages/MeetingListPage/index.tsx
@@ -1,13 +1,16 @@
+import { css } from '@emotion/react';
 import * as S from './MeetingListPage.styled';
-import Button from 'components/@shared/Button';
 import Footer from 'components/layouts/Footer';
 import MeetingItem from 'components/MeetingItem';
 import CoffeeStackItem from 'components/CoffeeStackItem';
 import Spinner from 'components/@shared/Spinner';
-import { MeetingListResponseBody } from 'types/meetingType';
-import useFetch from 'hooks/useFetch';
 import ErrorIcon from 'components/@shared/ErrorIcon';
 import ReloadButton from 'components/@shared/ReloadButton';
+import DivideLine from 'components/@shared/DivideLine';
+import Button from 'components/@shared/Button';
+import useFetch from 'hooks/useFetch';
+import NoSearchResultIconSVG from 'assets/NoSearchResult.svg';
+import { MeetingListResponseBody } from 'types/meetingType';
 
 const MeetingListPage = () => {
   const {
@@ -48,6 +51,33 @@ const MeetingListPage = () => {
     );
   }
 
+  if (meetingListState.meetings.length === 0) {
+    return (
+      <>
+        <S.Layout>
+          <S.EmptyStateBox>
+            <S.EmptyStateImage src={NoSearchResultIconSVG} alt="no meetings" />
+            <S.EmptyStateTitle>아직 모임이 없어요.</S.EmptyStateTitle>
+            <S.EmptyStateParagraph>
+              직접 모임을 등록하러 가볼까요?
+            </S.EmptyStateParagraph>
+            <DivideLine
+              css={css`
+                width: 75%;
+                margin-top: 1rem;
+                margin-bottom: 0.5rem;
+              `}
+            />
+            <S.MeetingCreateLink to="/meeting/create">
+              모임 생성하기
+            </S.MeetingCreateLink>
+          </S.EmptyStateBox>
+        </S.Layout>
+        <Footer />
+      </>
+    );
+  }
+
   return (
     <>
       <S.Layout>
@@ -73,15 +103,11 @@ const MeetingListPage = () => {
             <S.PageLink to="/meeting/create">생성하기</S.PageLink>
           </S.TitleBox>
           <S.MeetingList>
-            {meetingListState.meetings.length > 0 ? (
-              meetingListState.meetings.map((meeting) => (
-                <li key={meeting.id}>
-                  <MeetingItem meeting={meeting} />
-                </li>
-              ))
-            ) : (
-              <div>참여 중인 모임이 없습니다.</div>
-            )}
+            {meetingListState.meetings.map((meeting) => (
+              <li key={meeting.id}>
+                <MeetingItem meeting={meeting} />
+              </li>
+            ))}
           </S.MeetingList>
         </S.MeetingListSection>
         <S.CoffeeStackSection>
