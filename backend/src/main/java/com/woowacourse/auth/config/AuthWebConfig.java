290c3c583d0c622877ed1180449a785f0f902818
YJGwon <yj970125@gmail.com>
2022-07-21 17:09:20 +0900

    feat: allow cross-origin

--- backend/src/main/java/com/woowacourse/auth/config/AuthWebConfig.java
+++ backend/src/main/java/com/woowacourse/auth/config/AuthWebConfig.java
@@ -2,16 +2,20 @@ package com.woowacourse.auth.config;
 
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AuthWebConfig implements WebMvcConfigurer {
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     private final List<HandlerInterceptor> interceptors;
     private final List<HandlerMethodArgumentResolver> resolvers;
+
 
     public AuthWebConfig(final List<HandlerInterceptor> interceptors,
                          final List<HandlerMethodArgumentResolver> resolvers) {
@@ -19,6 +23,13 @@ public class AuthWebConfig implements WebMvcConfigurer {
         this.resolvers = resolvers;
     }
 
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION);
+    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
