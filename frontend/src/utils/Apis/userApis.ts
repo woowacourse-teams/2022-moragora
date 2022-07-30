0251c2631ac387615c9ca561c3b007d184f62dc5
kamwoo <rkadndud06@naver.com>
2022-07-30 19:51:36 +0900

    feat: user관련 request 정의

---
+++ frontend/src/utils/Apis/userApis.ts
@@ -0,0 +1,92 @@
+import { UserContextValues } from 'contexts/userContext';
+import {
+  GetLoginUserDataResponseBody,
+  Participant,
+  User,
+  UserAttendanceCheckRequestBody,
+  UserLoginRequestBody,
+  UserLoginResponseBody,
+  UserRegisterRequestBody,
+} from 'types/userType';
+import request from '../request';
+
+type PutUserAttendanceApiParameter = {
+  meetingId: string;
+  userId: Participant['id'];
+  user: UserContextValues['user'];
+  AttendanceStatus: UserAttendanceCheckRequestBody['attendanceStatus'];
+};
+
+export const checkEmailApi = (email: User['email']) => () =>
+  request<{ isExist: boolean }>(`/users/check-email?email=${email}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const submitRegisterApi = async (payload: UserRegisterRequestBody) => {
+  await request<{}>('/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const { nickname, ...loginRequestBody } = payload;
+
+  return submitLoginApi(loginRequestBody);
+};
+
+export const submitLoginApi = async (payload: UserLoginRequestBody) => {
+  const loginResponse = await request<UserLoginResponseBody>('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!loginResponse.body.accessToken) {
+    throw new Error('로그인 중 오류가 발생했습니다.');
+  }
+
+  const accessToken = loginResponse.body.accessToken;
+  const loginUserResponse = await request<GetLoginUserDataResponseBody>(
+    '/users/me',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  return { ...loginUserResponse, accessToken };
+};
+
+export const putUserAttendanceApi = async ({
+  meetingId,
+  userId,
+  user,
+  AttendanceStatus,
+}: PutUserAttendanceApiParameter) => {
+  if (!user) {
+    throw new Error('유저가 존재하지 않습니다.');
+  }
+
+  if (!user.accessToken) {
+    throw new Error('미팅 정보를 불러오는 중 에러가 발생했습니다.');
+  }
+
+  return request<{}>(`/meetings/${meetingId}/users/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${user.accessToken}`,
+    },
+    body: JSON.stringify({ AttendanceStatus }),
+  });
+};
