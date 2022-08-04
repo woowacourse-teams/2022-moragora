26faeb2be8ee6c076e8236ec59269c39764ec280
Jeongmin Woo <greenblues1190@gmail.com>
2022-08-04 20:33:51 +0900

    chore: cypress 환경 설정

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+const webpackConfig = require('./webpack/webpack.test');
+
+console.log(webpackConfig);
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig,
     },
   },
 });
