836bf43437d84d844e3cbcf64f439fc39f538ec6
Forky <trulyforky@gmail.com>
2022-08-05 10:11:51 +0900

    [BE] 출석체크 요청에 대한 검증 추가 (#278)

--- backend/src/main/java/com/woowacourse/moragora/dto/UserAttendanceRequest.java
+++ backend/src/main/java/com/woowacourse/moragora/dto/UserAttendanceRequest.java
@@ -1,6 +1,7 @@
 package com.woowacourse.moragora.dto;
 
 import com.woowacourse.moragora.entity.Status;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -10,6 +11,9 @@ import lombok.ToString;
 @ToString
 public class UserAttendanceRequest {
 
+    private static final String MISSING_REQUIRED_INPUT = "필수 입력 값이 누락됐습니다.";
+
+    @NotNull(message = MISSING_REQUIRED_INPUT)
     private Status attendanceStatus;
 
     public UserAttendanceRequest(final Status attendanceStatus) {
