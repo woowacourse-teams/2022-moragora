931dc647a6379e08b1032c55e735fd1c673e5760
kamwoo <rkadndud06@naver.com>
2022-08-09 14:16:50 +0900

    feat: DeviceLayout 컴포넌트 작성

---
+++ frontend/src/components/layouts/DeviceLayout/DeviceLayout.styled.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const DeviceLayout = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
---
+++ frontend/src/components/layouts/DeviceLayout/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './DeviceLayout.styled';
+
+const DeviceLayout = S.DeviceLayout;
+
+export default DeviceLayout;
