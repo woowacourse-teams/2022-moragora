27cd046c8c056529a1770c2aca2bed19018e5e18
shindong96 <2006peter@naver.com>
2022-08-11 14:58:18 +0900

    refactor: 스케줄러 제거에 따라 Repository 강제 flush 제거

--- backend/src/main/java/com/woowacourse/moragora/repository/AttendanceRepository.java
+++ backend/src/main/java/com/woowacourse/moragora/repository/AttendanceRepository.java
@@ -3,10 +3,7 @@ package com.woowacourse.moragora.repository;
 import com.woowacourse.moragora.entity.Attendance;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends Repository<Attendance, Long> {
 
@@ -20,7 +17,5 @@ public interface AttendanceRepository extends Repository<Attendance, Long> {
 
     List<Attendance> findByParticipantIdInAndEventId(final List<Long> participantIds, final Long eventId);
 
-    @Modifying
-    @Query("delete from Attendance a where a.event.id in :eventIds")
-    void deleteByEventIdIn(@Param("eventIds") final List<Long> eventIds);
+    void deleteByEventIdIn(final List<Long> eventIds);
 }
