d43687d052dc1e269cdab1a70aeff8e34da52a3f
Jeongmin Woo <greenblues1190@gmail.com>
2022-08-11 14:09:08 +0900

    [FE] API 수정, 추가 사항 mocking 반영 (#320)
    * feat: API 수정 및 추가 사항 추가 모킹
    
    * refactor: nickname 타입 대소문자 수정

---
+++ frontend/src/mocks/fixtures/meetings_new.ts
@@ -0,0 +1,62 @@
+import { Meeting } from 'types/meetingType_new';
+
+const meetings: Meeting[] = [
+  {
+    id: 1,
+    name: '리액트 사랑방',
+    isActive: false,
+    userIds: [1, 2, 3, 4, 5],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 2,
+    name: '우테코 레벨3 반상회',
+    isActive: false,
+    userIds: [1, 2, 3, 4, 5, 6],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 3,
+    name: '이게모임',
+    isActive: false,
+    userIds: [1, 3, 4, 5, 6, 7],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 4,
+    name: '2022 우아한테크코스 백엔드4기 비장의 토비의 스프링 스터디',
+    isActive: false,
+    userIds: [1, 4, 5, 6, 7, 8],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 5,
+    name: '저건모임',
+    isActive: false,
+    userIds: [1, 5, 6, 7, 8, 9],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 6,
+    name: '모임6',
+    isActive: false,
+    userIds: [1, 6, 7, 8, 9, 10],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 7,
+    name: '모임7',
+    isActive: false,
+    userIds: [1, 7, 8, 9, 10, 11],
+    attendanceEventCount: 10,
+  },
+  {
+    id: 8,
+    name: '사단법인 싱겁게먹기실천연구회',
+    isActive: false,
+    userIds: [1, 8, 9, 10, 11, 12],
+    attendanceEventCount: 10,
+  },
+];
+
+export default meetings;
