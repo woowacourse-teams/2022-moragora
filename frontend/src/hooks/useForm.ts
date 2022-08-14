45d5c7f2f104a4c335b9ce2a577cb56384f22535
Jeongmin Woo <greenblues1190@gmail.com>
2022-08-15 05:00:39 +0900

    feat: useForm에 customValidation 추가

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -1,20 +1,46 @@
 import React, { useRef, useState } from 'react';
 
-type Values = Record<HTMLInputElement['name'], HTMLInputElement['value']>;
-type Errors = Record<HTMLInputElement['name'], string>;
+type Name = React.InputHTMLAttributes<HTMLInputElement>['name'];
+type Value = React.InputHTMLAttributes<HTMLInputElement>['value'];
+type Error = HTMLInputElement['validationMessage'];
+type Values = Record<NonNullable<Name>, Value>;
+type Errors = Record<NonNullable<Name>, Error>;
+type Validation = {
+  validate: (value: HTMLInputElement['value']) => boolean;
+  validationMessage: HTMLInputElement['validationMessage'];
+};
+type InputAttributes = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'name'
+> &
+  Partial<{
+    watch: boolean;
+    patternValidationMessage: HTMLInputElement['validationMessage'];
+    customValidations: Validation[];
+  }>;
+type InputController = Record<
+  NonNullable<Name>,
+  {
+    element: HTMLInputElement;
+    checkValidity: HTMLInputElement['checkValidity'];
+  }
+>;
 
 const useForm = () => {
   const [values, setValues] = useState<Values>({});
   const [errors, setErrors] = useState<Errors>({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const inputController = useRef<InputController>({});
   const inputElementList = useRef<HTMLInputElement[]>([]);
 
-  const validate = (target: HTMLInputElement) => {
-    target.checkValidity();
+  const validateInput = (target: HTMLInputElement) => {
+    const targetInputElement = inputController.current[target.name];
+
+    targetInputElement.checkValidity();
 
     setErrors((prev) => ({
       ...prev,
-      [target.name]: target.validationMessage,
+      [target.name]: targetInputElement.element.validationMessage,
     }));
   };
 
@@ -23,26 +49,27 @@ const useForm = () => {
       onChange?: React.ChangeEventHandler<HTMLInputElement>
     ): React.ChangeEventHandler<HTMLInputElement> =>
     (e) => {
-      const { target } = e;
-      const { name, value } = target;
+      const { currentTarget } = e;
+      const { name, value } = currentTarget;
 
-      validate(target);
-
+      validateInput(currentTarget);
       setValues((prev) => ({
         ...prev,
         [name]: value,
       }));
-
-      if (onChange) {
-        onChange(e);
-      }
+      onChange?.(e);
     };
 
-  const handleBlur: React.FormEventHandler<HTMLInputElement> = (e) => {
-    const { target } = e;
+  const handleBlur =
+    (
+      onBlur?: React.FocusEventHandler<HTMLInputElement>
+    ): React.FocusEventHandler<HTMLInputElement> =>
+    (e) => {
+      const { currentTarget } = e;
 
-    validate(target as HTMLInputElement);
-  };
+      validateInput(currentTarget);
+      onBlur?.(e);
+    };
 
   const handleSubmit =
     (
@@ -53,7 +80,7 @@ const useForm = () => {
       e.preventDefault();
 
       inputElementList.current.forEach((element) => {
-        validate(element);
+        validateInput(element);
       });
 
       const isValidateComplete =
@@ -69,11 +96,11 @@ const useForm = () => {
           onValid(e);
 
           return;
-        } else if (onError) {
-          onError(e);
         }
+
+        throw e;
       } catch (e) {
-        console.error(e);
+        onError?.(e as React.FormEvent<HTMLFormElement>);
       } finally {
         setIsSubmitting(false);
       }
@@ -89,27 +116,98 @@ const useForm = () => {
     };
   };
 
-  const bindInputElementToRef: React.RefCallback<HTMLInputElement> = (
-    element
-  ) => {
-    if (element && !inputElementList.current.includes(element)) {
+  const bindInputElementToRef =
+    (
+      patternValidationMessage: InputAttributes['patternValidationMessage'],
+      customValidations: InputAttributes['customValidations']
+    ): React.RefCallback<HTMLInputElement> =>
+    (element) => {
+      if (!element || inputController.current[element.name]) {
+        return;
+      }
+
       inputElementList.current.push(element);
-    }
-  };
+
+      const setValidClass = () => {
+        element.classList.add('valid');
+        element.classList.remove('invalid');
+      };
+
+      const setInvalidClass = () => {
+        element.classList.add('invalid');
+        element.classList.remove('valid');
+      };
+
+      const checkCustomValidity = (
+        customValidations: NonNullable<InputAttributes['customValidations']>
+      ) =>
+        customValidations.every(({ validate, validationMessage }) => {
+          const customValidationValidity = validate(element.value);
+
+          element.setCustomValidity(
+            customValidationValidity ? '' : validationMessage
+          );
+
+          return customValidationValidity;
+        });
+
+      const checkValidity = () => {
+        element.setCustomValidity('');
+
+        let isValid = element.checkValidity();
+
+        if (element.validity.patternMismatch && patternValidationMessage) {
+          element.setCustomValidity(patternValidationMessage);
+        }
+
+        if (isValid && customValidations) {
+          isValid = checkCustomValidity(customValidations);
+        }
+
+        if (isValid) {
+          setValidClass();
+        } else {
+          setInvalidClass();
+        }
+
+        return isValid;
+      };
+
+      inputController.current[element.name] = {
+        element,
+        checkValidity,
+      };
+    };
 
   const register = (
     name: HTMLInputElement['name'],
     {
+      watch,
+      defaultValue,
       onChange,
+      onBlur,
+      patternValidationMessage,
+      customValidations,
       ...attributes
-    }: Omit<React.InputHTMLAttributes<HTMLInputElement>, 'name'>
+    }: InputAttributes
   ) => {
+    const shouldWatch =
+      watch && !Object.prototype.hasOwnProperty.call(values, name);
+
+    if (shouldWatch) {
+      setValues((prev) => ({
+        ...prev,
+        [name]: defaultValue ?? '',
+      }));
+    }
+
     return {
       name,
-      ...attributes,
-      ref: bindInputElementToRef,
+      defaultValue,
+      ref: bindInputElementToRef(patternValidationMessage, customValidations),
       onChange: handleChange(onChange),
-      onBlur: handleBlur,
+      onBlur: handleBlur(onBlur),
+      ...attributes,
     };
   };
 
