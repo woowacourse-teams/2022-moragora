be2e8eeab5b223491ec00bac0ff4394d6221c1b8
kamwoo <rkadndud06@naver.com>
2022-08-17 11:22:00 +0900

    feat: coffeeStackItem props 수정 및 master icon 추가

--- frontend/src/components/CoffeeStackItem/index.tsx
+++ frontend/src/components/CoffeeStackItem/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import * as S from './CoffeeStackItem.styled';
-import CoffeeIconSVG from '../../assets/coffee.svg';
-import { MeetingWithTardyCount } from 'types/meetingType';
+import CoffeeIconSVG from 'assets/coffee.svg';
+import CrownIconSVG from 'assets/crown.svg';
+import { Participant } from 'types/userType_new';
+import { Meeting } from 'types/meetingType_new';
 
-type CoffeeStackItemProps = Pick<MeetingWithTardyCount, 'name' | 'tardyCount'>;
+type CoffeeStackItemProps = {
+  name: Participant['nickname'] | Meeting['name'];
+  tardyCount: Participant['tardyCount'];
+  isMaster: boolean;
+};
 
 const CoffeeStackItem: React.FC<CoffeeStackItemProps> = ({
   name,
   tardyCount,
+  isMaster = false,
 }) => {
   return (
     <S.Layout>
-      <span>{name}</span>
-      <S.Box>
+      <S.NameBox>
+        <span>{name}</span>
+        {isMaster && <img src={CrownIconSVG} alt="crown-icon" width={20} />}
+      </S.NameBox>
+      <S.CoffeeStackBox>
         {Array.from({ length: tardyCount }).map((_, index) => (
           <S.CoffeeIconImage key={index} src={CoffeeIconSVG} />
         ))}
-      </S.Box>
+      </S.CoffeeStackBox>
     </S.Layout>
   );
 };
