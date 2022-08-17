1dd55beabf25deb3bf88fb5b147a5a39f3d078cd
kamwoo <rkadndud06@naver.com>
2022-08-17 22:07:06 +0900

    feat: check-in 페이지 path 추가 및 네비게이션 바 탭 추가

--- frontend/src/components/layouts/Footer/index.tsx
+++ frontend/src/components/layouts/Footer/index.tsx
@@ -37,6 +37,32 @@ const Footer: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
           <S.Figcaption>모임</S.Figcaption>
         </S.Figure>
       </S.MenuNavLink>
+      <S.MenuNavLink to="/check-in">
+        <S.Figure>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.665 3.5C5.135 3.5 3.5 5.233 3.5 7.916V16.084C3.5 18.767 5.135 20.5 7.665 20.5H16.333C18.864 20.5 20.5 18.767 20.5 16.084V7.916C20.5 5.233 18.864 3.5 16.334 3.5H7.665ZM16.333 22H7.665C4.276 22 2 19.622 2 16.084V7.916C2 4.378 4.276 2 7.665 2H16.334C19.723 2 22 4.378 22 7.916V16.084C22 19.622 19.723 22 16.333 22Z"
+              fill="currentFill"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M10.8134 15.123C10.6224 15.123 10.4294 15.05 10.2834 14.903L7.90945 12.53C7.61645 12.237 7.61645 11.763 7.90945 11.47C8.20245 11.177 8.67645 11.177 8.96945 11.47L10.8134 13.312L15.0294 9.09701C15.3224 8.80401 15.7964 8.80401 16.0894 9.09701C16.3824 9.39001 16.3824 9.86401 16.0894 10.157L11.3434 14.903C11.1974 15.05 11.0054 15.123 10.8134 15.123Z"
+              fill="currentFill"
+            />
+          </svg>
+
+          <S.Figcaption>출첵</S.Figcaption>
+        </S.Figure>
+      </S.MenuNavLink>
       <S.MenuNavLink to="/settings">
         <S.Figure>
           <svg
