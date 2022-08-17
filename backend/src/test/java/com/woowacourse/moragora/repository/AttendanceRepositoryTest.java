d9702502c8befbc1854382f2ad3fc3162ad6ee79
YJGwon <yj970125@gmail.com>
2022-08-17 16:20:13 +0900

    test(AttendanceRepositoryTest): 참가자 출석 데이터 없을 때 삭제 기능 검증, DataSupport 적용

--- backend/src/test/java/com/woowacourse/moragora/repository/AttendanceRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/moragora/repository/AttendanceRepositoryTest.java
@@ -5,6 +5,7 @@ import static com.woowacourse.moragora.support.MeetingFixtures.MORAGORA;
 import static com.woowacourse.moragora.support.UserFixtures.AZPI;
 import static com.woowacourse.moragora.support.UserFixtures.KUN;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.woowacourse.moragora.entity.Attendance;
 import com.woowacourse.moragora.entity.Event;
@@ -32,25 +33,18 @@ class AttendanceRepositoryTest {
     @Autowired
     private DataSupport dataSupport;
 
-    @Autowired
-    private EventRepository eventRepository;
-
     @DisplayName("미팅 참가자의 해당 날짜 출석정보를 조회한다.")
     @Test
     void findByParticipantIdAndEventId() {
         // given
         final Meeting meeting = MORAGORA.create();
         final Participant participant = dataSupport.saveParticipant(KUN.create(), meeting, false);
-        final Event event1 = EVENT1.create(meeting);
-        final Event savedEvent = eventRepository.save(event1);
-
-        attendanceRepository.save(new Attendance(Status.TARDY, false, participant, savedEvent));
-
-        final Optional<Event> event = eventRepository.findByMeetingIdAndDate(meeting.getId(), savedEvent.getDate());
+        final Event event = dataSupport.saveEvent(EVENT1.create(meeting));
+        dataSupport.saveAttendance(participant, event, Status.TARDY);
 
         // when
         final Optional<Attendance> attendance = attendanceRepository
-                .findByParticipantIdAndEventId(participant.getId(), event.get().getId());
+                .findByParticipantIdAndEventId(participant.getId(), event.getId());
 
         // then
         assertThat(attendance.isPresent()).isTrue();
@@ -62,20 +56,15 @@ class AttendanceRepositoryTest {
         // given
         final User user1 = KUN.create();
         final User user2 = AZPI.create();
-
-        final Meeting meeting = dataSupport.saveMeeting(MORAGORA.create());
+        final Meeting meeting = MORAGORA.create();
 
         final Participant participant1 = dataSupport.saveParticipant(user1, meeting);
         final Participant participant2 = dataSupport.saveParticipant(user2, meeting);
-
-        final Event event = EVENT1.create(meeting);
-        final Event savedEvent = eventRepository.save(event);
-
-        attendanceRepository.save(new Attendance(Status.TARDY, true, participant1, savedEvent));
-        attendanceRepository.save(new Attendance(Status.TARDY, true, participant2, savedEvent));
+        final Event event = dataSupport.saveEvent(EVENT1.create(meeting));
+        dataSupport.saveAttendance(participant1, event, Status.TARDY);
+        dataSupport.saveAttendance(participant2, event, Status.TARDY);
 
         final List<Participant> participants = List.of(participant1, participant2);
-
         final List<Long> participantIds = participants.stream()
                 .map(Participant::getId)
                 .collect(Collectors.toList());
@@ -93,16 +82,12 @@ class AttendanceRepositoryTest {
         // given
         final User user1 = KUN.create();
         final User user2 = AZPI.create();
-
-        final Meeting meeting = dataSupport.saveMeeting(MORAGORA.create());
+        final Meeting meeting = MORAGORA.create();
 
         final Participant participant1 = dataSupport.saveParticipant(user1, meeting);
         final Participant participant2 = dataSupport.saveParticipant(user2, meeting);
-
-        final Event event = EVENT1.create(meeting);
-        final Event savedEvent = eventRepository.save(event);
-
-        final Attendance attendance1 = dataSupport.saveAttendance(participant1, savedEvent, Status.TARDY);
+        final Event event = dataSupport.saveEvent(EVENT1.create(meeting));
+        final Attendance attendance = dataSupport.saveAttendance(participant1, event, Status.TARDY);
 
         final List<Participant> participants = List.of(participant1, participant2);
 
@@ -112,7 +97,7 @@ class AttendanceRepositoryTest {
 
         // when
         final List<Attendance> attendances =
-                attendanceRepository.findByParticipantIdInAndEventId(participantIds, attendance1.getEvent().getId());
+                attendanceRepository.findByParticipantIdInAndEventId(participantIds, attendance.getEvent().getId());
 
         // then
         assertThat(attendances).hasSize(1);
@@ -123,9 +108,9 @@ class AttendanceRepositoryTest {
     void deleteByParticipantId() {
         // given
         final User user = KUN.create();
-        final Meeting meeting = dataSupport.saveMeeting(MORAGORA.create());
+        final Meeting meeting = MORAGORA.create();
         final Participant participant = dataSupport.saveParticipant(user, meeting);
-        final Event event = eventRepository.save(EVENT1.create(meeting));
+        final Event event = dataSupport.saveEvent(EVENT1.create(meeting));
         dataSupport.saveAttendance(participant, event, Status.TARDY);
 
         // when
@@ -136,4 +121,18 @@ class AttendanceRepositoryTest {
         assertThat(result).hasSize(0);
     }
 
+    @DisplayName("해당 참가자의 출석 데이터가 존재하지 않을 경우에 삭제해도 예외가 발생하지 않는다.")
+    @Test
+    void deleteByParticipantId_ifNotExist() {
+        // given
+        final User user = KUN.create();
+        final Meeting meeting = MORAGORA.create();
+        // 왜 되는지 알겠음 ~ save는 persist or merge라서 당연히 됨~
+        final Participant participant = dataSupport.saveParticipant(user, meeting);
+        dataSupport.saveEvent(EVENT1.create(meeting));
+
+        // when, then
+        assertThatCode(() -> attendanceRepository.deleteByParticipantId(participant.getId()))
+                .doesNotThrowAnyException();
+    }
 }
