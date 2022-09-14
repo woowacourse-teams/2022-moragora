03847b1538e58f4eceffc9f7e16a9a76f65fb11c
Jeongmin Woo <greenblues1190@gmail.com>
2022-09-14 16:16:50 +0900

    chore: production 모드 webpack-bundle-analyzer 설정
    - `analyzerMode`를 `disabled`로 설정하고 `generateStatsFile`을 `true`로 해 json 파일을 출력한다.
    - `reportFilename`을 `stats/stats-${currentDate}.json`로 설정한다.
    - `currentDate`는 읽기의 편의를 위해 ISO 8601 포맷에서 밀리세컨드를 제거한다.

--- frontend/webpack/webpack.prod.js
+++ frontend/webpack/webpack.prod.js
@@ -1,6 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const currentDate = new Date().toISOString().split('.')[0];
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+      statsFilename: `stats/stats-${currentDate}.json`,
+    }),
+  ],
 });
