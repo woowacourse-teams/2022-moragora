e9e8403630f127be6825f8631526ea3b243c55fd
Hongdonggeon <ghd700@naver.com>
2022-09-16 12:48:38 +0900

    test: Event 엔티티에 대한 테스트 추가

---
+++ backend/src/test/java/com/woowacourse/moragora/entity/EventTest.java
@@ -0,0 +1,81 @@
+package com.woowacourse.moragora.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.woowacourse.moragora.support.EventFixtures;
+import com.woowacourse.moragora.support.MeetingFixtures;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    @DisplayName("같은 날짜의 이벤트인지 확인한다.")
+    @Test
+    void isSameDate() {
+        // given
+        final Meeting meeting = MeetingFixtures.MORAGORA.create();
+        final Event event = new Event(
+                LocalDate.of(2022, 8, 1),
+                LocalTime.of(10, 0),
+                LocalTime.of(18, 0),
+                meeting);
+
+        // when
+        final boolean actual = event.isSameDate(LocalDate.of(2022, 8, 1));
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("이벤트를 가지고 있는 미팅이 같은지 확인한다.")
+    @Test
+    void isSameMeeting() {
+        // given
+        final Meeting meeting = MeetingFixtures.MORAGORA.create();
+        final Event event = EventFixtures.EVENT1.create(meeting);
+
+        // when
+        final boolean actual = event.isSameMeeting(meeting);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("이벤트의 시간을 업데이트한다.")
+    @Test
+    void updateTime() {
+        // given
+        final Meeting meeting = MeetingFixtures.MORAGORA.create();
+        final Event event = EventFixtures.EVENT1.create(meeting);
+
+        // when
+        event.updateTime(LocalTime.of(10, 5), LocalTime.of(11, 5));
+
+        // then
+        assertAll(
+                () -> assertThat(event.getStartTime()).isEqualTo(LocalTime.of(10, 5)),
+                () -> assertThat(event.getEndTime()).isEqualTo(LocalTime.of(11, 5))
+        );
+    }
+
+    @DisplayName("이벤트가 이전 날짜인지 확인한다.")
+    @Test
+    void isDateBefore() {
+        // given
+        final Meeting meeting = MeetingFixtures.MORAGORA.create();
+        final Event event = new Event(
+                LocalDate.of(2022, 8, 1),
+                LocalTime.of(10, 0),
+                LocalTime.of(18, 0),
+                meeting);
+
+        // when
+        final boolean actual = event.isDateBefore(LocalDate.of(2022, 8, 2));
+
+        // then
+        assertThat(actual).isTrue();
+    }
+}
