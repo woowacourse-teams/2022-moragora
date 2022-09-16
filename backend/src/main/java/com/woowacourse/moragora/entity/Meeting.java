492ae29eaa29990f71b21bb24a27709d608e1a3e
Hongdonggeon <67885363+Hongdonggeon@users.noreply.github.com>
2022-09-16 16:27:36 +0900

    [BE] 누락된 엔티티 단위 테스트 추가 (#425)
    * test: Attendance entity 테스트 추가
    
    * test: Event 엔티티에 대한 테스트 추가
    
    * test: ParticipantAttendances 단위 테스트 추가
    
    * refactor: meeting 엔티티 equals&hashcode 롬복 어노테이션으로 변경
    
    * test: MeetingAttendances 미팅수 검증 테스트 추가
    
    * test: ParticipantAttendancesTest displayname 변경
    
    * test: event 업데이트할 때 시간 검증 테스트 추가
    
    * test: Participant mapMeeting 테스트 추가
    
    * refactor: equals hashcode 선언 방법 변경

--- backend/src/main/java/com/woowacourse/moragora/entity/Meeting.java
+++ backend/src/main/java/com/woowacourse/moragora/entity/Meeting.java
@@ -3,7 +3,6 @@ package com.woowacourse.moragora.entity;
 import com.woowacourse.moragora.exception.InvalidFormatException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,6 +14,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,12 +23,14 @@ import lombok.NoArgsConstructor;
 @Table(name = "meeting")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Meeting {
 
     private static final int MAX_NAME_LENGTH = 50;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
     private Long id;
 
     @Column(nullable = false)
@@ -37,9 +40,14 @@ public class Meeting {
     private final List<Participant> participants = new ArrayList<>();
 
     @Builder
-    public Meeting(final String name) {
+    public Meeting(final Long id, final String name) {
         validateName(name);
+        this.id = id;
         this.name = name;
+    }
+
+    public Meeting(final String name) {
+        this(null, name);
     }
 
     public void updateName(final String name) {
@@ -57,22 +65,5 @@ public class Meeting {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new InvalidFormatException();
         }
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Meeting)) {
-            return false;
-        }
-        final Meeting meeting = (Meeting) o;
-        return Objects.equals(this.id, meeting.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
     }
 }
