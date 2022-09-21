850dc90b2ea9f5c7d742caa5f045eaa0794287ea
kamwoo <rkadndud06@naver.com>
2022-09-21 16:33:47 +0900

    feat: login page height 설정

--- frontend/src/pages/LoginPage/LoginPage.styled.ts
+++ frontend/src/pages/LoginPage/LoginPage.styled.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 export const Layout = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
   overflow: hidden;
