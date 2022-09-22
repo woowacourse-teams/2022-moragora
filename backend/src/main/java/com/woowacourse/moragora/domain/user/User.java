a116954967354a672a9e7a8d821d2f700adfac84
shindong96 <2006peter@naver.com>
2022-09-22 19:39:35 +0900

    feat: User 테이블 email로 인덱싱

--- backend/src/main/java/com/woowacourse/moragora/domain/user/User.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/user/User.java
@@ -16,6 +16,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,7 +24,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user", indexes = @Index(name = "idx_email", columnList = "email"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class User {
