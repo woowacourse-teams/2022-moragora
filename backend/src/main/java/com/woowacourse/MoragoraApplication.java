0472d1661a149e231d0bb9a7b5ac0d58e234c430
Hongdonggeon <ghd700@naver.com>
2022-09-22 15:10:08 +0900

    CI 테스트

--- backend/src/main/java/com/woowacourse/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/MoragoraApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoragoraApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(MoragoraApplication.class, args);
     }
