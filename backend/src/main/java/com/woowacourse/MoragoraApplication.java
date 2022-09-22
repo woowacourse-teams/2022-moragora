9b63a17ae8388fafe45335075f7a41af26483660
Hongdonggeon <ghd700@naver.com>
2022-09-22 19:12:41 +0900

    refactor: 불필요한 공백 제거

--- backend/src/main/java/com/woowacourse/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/MoragoraApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoragoraApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(MoragoraApplication.class, args);
     }
