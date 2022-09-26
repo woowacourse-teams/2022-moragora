2f7e0ca90d67de41a26a85306648e71a660dd759
syoun602 <syoun602@gmail.com>
2022-09-26 16:37:44 +0900

    refactor: 파라미터 이름 수정

--- backend/src/main/java/com/woowacourse/moragora/application/ServerTimeManager.java
+++ backend/src/main/java/com/woowacourse/moragora/application/ServerTimeManager.java
@@ -33,8 +33,8 @@ public class ServerTimeManager {
         return dateTime.isAfter(closingTime);
     }
 
-    public boolean isAfter(final LocalTime startTime) {
-        return dateTime.isAfter(startTime);
+    public boolean isAfter(final LocalTime localTime) {
+        return dateTime.isAfter(localTime);
     }
 
     public LocalTime calculateOpenTime(final LocalTime meetingStartTime) {
