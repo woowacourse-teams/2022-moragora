1ef8a4e0be57a04d9d1573fba97ef6281438486d
Hongdonggeon <ghd700@naver.com>
2022-10-04 17:43:57 +0900

    refactor: DatabaseCleanUp 어노테이션 변경

--- backend/src/test/java/com/woowacourse/moragora/support/DatabaseCleanUp.java
+++ backend/src/test/java/com/woowacourse/moragora/support/DatabaseCleanUp.java
@@ -6,10 +6,10 @@ import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+@Component
 public class DatabaseCleanUp {
 
     @PersistenceContext
