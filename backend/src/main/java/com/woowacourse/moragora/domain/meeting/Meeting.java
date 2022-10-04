32d48920c162bdfe838fe70b750a696d20335c5a
syoun602 <syoun602@gmail.com>
2022-10-04 17:28:37 +0900

    refactor: ManyToOne FetchType Lazy로 일괄 수정

--- backend/src/main/java/com/woowacourse/moragora/domain/meeting/Meeting.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/meeting/Meeting.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,7 +27,7 @@ import lombok.NoArgsConstructor;
 public class Meeting {
 
     private static final int MAX_NAME_LENGTH = 50;
-    @OneToMany(mappedBy = "meeting", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "meeting")
     private final List<Participant> participants = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
