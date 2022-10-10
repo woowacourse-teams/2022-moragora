3f6fc813110031204b6f4e6cb41824cfbba4f918
progress0407 <progress0407@gmail.com>
2022-10-10 17:59:53 +0900

    success version

---
+++ backend/src/test/java/com/woowacourse/moragora/HelloTest.java
@@ -0,0 +1,12 @@
+package com.woowacourse.moragora;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    void hello() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
