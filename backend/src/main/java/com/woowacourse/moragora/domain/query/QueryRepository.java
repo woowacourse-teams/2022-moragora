4899def9f5079406485bbe317c31e4fd3d1ec60a
syoun602 <syoun602@gmail.com>
2022-10-11 10:25:33 +0900

    refactor: query 포맷 변경

--- backend/src/main/java/com/woowacourse/moragora/domain/query/QueryRepository.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/query/QueryRepository.java
@@ -11,9 +11,11 @@ import org.springframework.data.repository.query.Param;
 
 public interface QueryRepository extends Repository<Meeting, Long> {
 
-    @Query("select p as participant, (select count(a) from Attendance a inner join a.event e "
-            + "  where a.participant.id = p.id and a.status = 'TARDY' and a.disabled = false and e.date <= :date) "
-            + "  as tardyCount "
+    @Query("select p as participant, "
+            +   "(select count(a) "
+            +   "from Attendance a inner join a.event e "
+            +   "where a.participant.id = p.id and a.status = 'TARDY' and a.disabled = false and e.date <= :date) "
+            +   "as tardyCount "
             + "from Participant p join fetch p.user "
             + "where p in :participants")
     List<ParticipantAndCount> countParticipantsTardy(@Param("participants") final List<Participant> participants,
