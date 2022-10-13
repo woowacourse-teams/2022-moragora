ba0b7e8963628f20ae906c21ed5f3285941a17e8
progress0407 <progress0407@gmail.com>
2022-10-13 12:20:34 +0900

    test(Event): getOpen, getCloseTime 추가

--- backend/src/main/java/com/woowacourse/moragora/domain/meeting/MeetingRepository.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/meeting/MeetingRepository.java
@@ -1,5 +1,6 @@
 package com.woowacourse.moragora.domain.meeting;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +17,12 @@ public interface MeetingRepository extends Repository<Meeting, Long> {
 
     @Query("select m from Meeting m join fetch m.participants p where m.id = :id")
     Optional<Meeting> findMeetingAndParticipantsById(@Param("id") final Long id);
+
+    @Query("select m "
+            + " from Meeting m "
+            + " join fetch m.participants p "
+            + " where p.user.id = :userId")
+    List<Meeting> findMeetingParticipantsByUserId(@Param("userId") Long userId);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query("delete from Meeting m where m.id = :id")
