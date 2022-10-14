9651912da900c9a108674c67f20b061a0b0c3ab9
kamwoo <rkadndud06@naver.com>
2022-10-14 15:39:54 +0900

    feat: appLayout height 적용

--- frontend/src/components/layouts/AppLayout/AppLayout.styled.ts
+++ frontend/src/components/layouts/AppLayout/AppLayout.styled.ts
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
 
+const appHeight = window.innerHeight / 16;
+
 export const AppLayout = styled.div`
   position: relative;
-  padding: 5rem 0;
+  height: ${appHeight - 9}rem;
+  padding: 5rem 0 4rem;
   background-color: ${({ theme: { colors } }) => colors['surface']};
 `;
--- frontend/src/components/layouts/Body/Body.styled.ts
+++ frontend/src/components/layouts/Body/Body.styled.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
 
 export const Body = styled.main`
-  height: calc(100vh - 10rem);
+  height: 100%;
 `;
