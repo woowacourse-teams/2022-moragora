a78e88e388968be65e79f395594c10e5b9f971a2
syoun602 <syoun602@gmail.com>
2022-10-14 15:35:09 +0900

    fix: 유효하지 않은 refresh token 삭제하는 기능 추가

--- backend/src/main/java/com/woowacourse/moragora/application/auth/AuthService.java
+++ backend/src/main/java/com/woowacourse/moragora/application/auth/AuthService.java
@@ -76,27 +76,44 @@ public class AuthService {
         return participant.getIsMaster();
     }
 
+    public TokenResponse refreshTokens(final String oldToken) {
+        final RefreshToken refreshToken = refreshTokenProvider.findRefreshToken(oldToken)
+                .orElseThrow(InvalidTokenException::ofInvalid);
+        validateRefreshTokenExpiration(refreshToken);
+        validateUserExists(refreshToken);
+
+        final TokenResponse tokenResponse = createTokenResponse(refreshToken.getUserId());
+        refreshTokenProvider.remove(oldToken);
+
+        return tokenResponse;
+    }
+
     private User saveGoogleUser(final GoogleProfileResponse profileResponse) {
         final User userToSave = new User(profileResponse.getEmail(), profileResponse.getName(), GOOGLE);
         return userRepository.save(userToSave);
     }
 
-    public TokenResponse refreshTokens(final String oldToken) {
-        final RefreshToken refreshToken = refreshTokenProvider.findRefreshToken(oldToken)
-                .orElseThrow(InvalidTokenException::ofInvalid);
-        validateRefreshTokenExpiration(refreshToken);
-
-        final Long userId = refreshToken.getUserId();
-        final String newAccessToken = jwtTokenProvider.createToken(String.valueOf(userId));
-        final String newRefreshToken = refreshTokenProvider.create(userId, serverTimeManager.getDateAndTime());
-        refreshTokenProvider.remove(oldToken);
-
-        return new TokenResponse(newAccessToken, newRefreshToken);
-    }
-
     private void validateRefreshTokenExpiration(final RefreshToken refreshToken) {
         if (refreshToken.isExpired(serverTimeManager.getDateAndTime())) {
-            throw InvalidTokenException.ofInvalid();
+            removeTokenAndThrowException(refreshToken);
         }
+    }
+
+    private void validateUserExists(final RefreshToken refreshToken) {
+        if (!userRepository.existsById(refreshToken.getUserId())) {
+            removeTokenAndThrowException(refreshToken);
+        }
+    }
+
+    private void removeTokenAndThrowException(final RefreshToken refreshToken) {
+        refreshTokenProvider.remove(refreshToken.getValue());
+        throw InvalidTokenException.ofInvalid();
+    }
+
+    private TokenResponse createTokenResponse(final Long userId) {
+        final String accessToken = jwtTokenProvider.createToken(String.valueOf(userId));
+        final String refreshToken = refreshTokenProvider.create(userId, serverTimeManager.getDateAndTime());
+
+        return new TokenResponse(accessToken, refreshToken);
     }
 }
