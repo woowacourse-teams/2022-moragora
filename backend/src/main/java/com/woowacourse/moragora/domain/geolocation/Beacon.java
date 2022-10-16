758598acd059936b9ee7f955287db228723b003f
progress0407 <progress0407@gmail.com>
2022-10-16 22:31:38 +0900

    refactor(Beacon): 좌표 계산 공식 변경

--- backend/src/main/java/com/woowacourse/moragora/domain/geolocation/Beacon.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/geolocation/Beacon.java
@@ -1,5 +1,11 @@
 package com.woowacourse.moragora.domain.geolocation;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
 import com.woowacourse.moragora.domain.meeting.Meeting;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -23,6 +29,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Beacon {
+
+    private static final double TO_RADIAN = Math.PI / 180.0;
+    private static final double TO_DEGREE = 180.0 / Math.PI;
+    public static final int EARTH_RADIUS = 6371;
+    public static final int TO_KILO_METER = 1_000;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -75,33 +86,33 @@ public class Beacon {
         this.meeting = meeting;
     }
 
+    /**
+     * 구면 좌표계 상의 두 지점 사이의 거리를 구합니다. (by Haversine Formula)
+     * <p>
+     * 거리 = 지구_반직경 * arc_hav(h)
+     * <p>
+     * Haversine을 풀어 놓은 공식을 사용한다
+     */
+    public double calculateDistance(final Beacon other) {
+
+        final double thisLatCos = cos(this.latitude * TO_RADIAN);
+        final double otherLatCos = cos(other.latitude * TO_RADIAN);
+
+        final double dLat = abs(this.latitude - other.latitude) * TO_RADIAN;
+        final double dLong = abs(this.longitude - other.longitude) * TO_RADIAN;
+
+        final double latHav = pow(sin(dLat / 2), 2.0);
+        final double longHav = pow(sin(dLong / 2), 2.0);
+
+        final double sqrt = sqrt(latHav + longHav * thisLatCos * otherLatCos);
+        final double distance = 2 * EARTH_RADIUS * sqrt * TO_KILO_METER;
+
+        return distance;
+    }
+
     public boolean isInRadius(final Beacon other) {
         final double distance = calculateDistance(other);
 
         return distance <= radius;
     }
-
-    public double calculateDistance(final Beacon other) {
-        double theta = this.latitude - other.latitude;
-
-        double dist =
-                Math.sin(deg2rad(this.latitude)) * Math.sin(deg2rad(other.latitude)) +
-                        Math.cos(deg2rad(this.latitude)) * Math.cos(deg2rad(other.latitude)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist *= (60 * 1.1515);
-        dist *= 1609.344;
-
-        return dist;
-    }
-
-    private double deg2rad(final double deg) {
-        return deg * Math.PI / 180;
-    }
-
-    // This function converts radians to decimal degrees
-    private static double rad2deg(double rad) {
-        return (rad * 180 / Math.PI);
-    }
-
 }
