a87bc739257bf2b45c5ddf8110927e275878280d
Jeongmin Woo <greenblues1190@gmail.com>
2022-10-17 22:01:29 +0900

    fix: fix jsx syntax

--- frontend/src/components/BeaconItem/index.tsx
+++ frontend/src/components/BeaconItem/index.tsx
@@ -36,17 +36,17 @@ const BeaconItem = ({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
             />
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
             />
           </svg>
@@ -57,12 +57,12 @@ const BeaconItem = ({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M19.5 12h-15"
             />
           </svg>
