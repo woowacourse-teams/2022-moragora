90ecb312198413e9bd7e588b850a8e7ab96c1e3a
kamwoo <rkadndud06@naver.com>
2022-10-17 11:55:58 +0900

    feat: app height resizing 추가 및 debounce 적용

--- frontend/src/components/layouts/AppLayout/AppLayout.styled.ts
+++ frontend/src/components/layouts/AppLayout/AppLayout.styled.ts
@@ -1,10 +1,8 @@
 import styled from '@emotion/styled';
 
-const appHeight = window.innerHeight / 16;
-
-export const AppLayout = styled.div`
+export const AppLayout = styled.div<{ appHeight: number }>`
   position: relative;
-  height: ${appHeight - 9}rem;
+  height: ${({ appHeight }) => appHeight / 16 - 9}rem;
   padding: 5rem 0 4rem;
   background-color: ${({ theme: { colors } }) => colors['surface']};
 `;
