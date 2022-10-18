e82fd5422c468a82f3fcffbefd6afad44a55aadb
kamwoo <rkadndud06@naver.com>
2022-10-18 16:21:18 +0900

    feat: mocking 중복 이메이 상태코드 변경

--- frontend/src/mocks/handlers/userHandler.ts
+++ frontend/src/mocks/handlers/userHandler.ts
@@ -47,7 +47,7 @@ export default [
 
       if (users.some((user) => user.email === email)) {
         return res(
-          ctx.status(400),
+          ctx.status(409),
           ctx.json({
             message: '중복된 이메일입니다.',
           }),
