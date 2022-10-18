b6dff7452d95aec5035366fac6efecbbc373268b
syoun602 <syoun602@gmail.com>
2022-10-18 13:44:51 +0900

    refactor: DatabaseCleanUp 방식 테이블 이름 스캔하도록 변경

--- backend/src/main/java/com/woowacourse/moragora/domain/auth/RefreshToken.java
+++ backend/src/main/java/com/woowacourse/moragora/domain/auth/RefreshToken.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "refreshtoken")
+@Table(name = "refresh_token")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
