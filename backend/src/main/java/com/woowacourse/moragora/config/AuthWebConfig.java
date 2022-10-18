23a2bb2a3a46be9fb6c73342c3cd822ee8e29ce6
YJGwon <trulyforky@gmail.com>
2022-10-18 14:58:49 +0900

    refactor: allowed origin 정보 yml로 이동

--- backend/src/main/java/com/woowacourse/moragora/config/AuthWebConfig.java
+++ backend/src/main/java/com/woowacourse/moragora/config/AuthWebConfig.java
@@ -1,6 +1,7 @@
 package com.woowacourse.moragora.config;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -15,18 +16,20 @@ public class AuthWebConfig implements WebMvcConfigurer {
 
     private final List<HandlerInterceptor> interceptors;
     private final List<HandlerMethodArgumentResolver> resolvers;
-
+    private final String allowedOrigin;
 
     public AuthWebConfig(final List<HandlerInterceptor> interceptors,
-                         final List<HandlerMethodArgumentResolver> resolvers) {
+                         final List<HandlerMethodArgumentResolver> resolvers,
+                         @Value("${cors-config.allowed-origin}") final String allowedOrigin) {
         this.interceptors = interceptors;
         this.resolvers = resolvers;
+        this.allowedOrigin = allowedOrigin;
     }
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://checkmate.today", "https://test.checkamte.today")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
