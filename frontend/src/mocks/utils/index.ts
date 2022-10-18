44e5e195546d07ede58b5632a308c05b00746476
kamwoo <rkadndud06@naver.com>
2022-10-19 03:24:47 +0900

    feat: accessToken 및 refreshToken extract 기능 구현

--- frontend/src/mocks/utils/index.ts
+++ frontend/src/mocks/utils/index.ts
@@ -1,28 +1,86 @@
 import { RestRequest } from 'msw';
 import { TOKEN_PREFIX } from 'mocks/configs';
 
-const extractIdFromToken = (token: string) =>
-  Number(token.replace(TOKEN_PREFIX, ''));
+const extractIdFromToken = (token: string) => Number(token.split('_')[1]);
 
-export const generateToken = (id: number) => `${TOKEN_PREFIX}${id}`;
+const extractExpiredTimestampFromAccessToken = (token: string) =>
+  Number(token.split('_'.slice(-1)));
 
-export const extractIdFromHeader = <T>(
-  req: RestRequest<T>
+const extractIdFromRefreshToken = (token: string) =>
+  Number(token.split('_')[2]);
+
+const extractExpiredTimestampFromRefreshToken = (token: string) =>
+  Number(token.split('_'.slice(-1)));
+
+export const generateAccessToken = (id: number) => {
+  const timer = new Date();
+  timer.setMinutes(timer.getMinutes() + 5);
+  const expiredTimestamp = timer.getTime();
+
+  return `${TOKEN_PREFIX}_${id}_${expiredTimestamp}`;
+};
+
+export const generateRefreshToken = (id: number) => {
+  const timer = new Date();
+  timer.setDate(timer.getDate() + 8);
+  const expiredTimestamp = timer.getTime();
+
+  return `${TOKEN_PREFIX}_refresh_${id}_${expiredTimestamp}`;
+};
+
+export const extractIdFromHeader = (
+  req: RestRequest
 ): {
   id?: number;
+  expiredTimestamp?: number;
   isValidToken: boolean;
 } => {
   const authorization = req.headers.get('Authorization');
   const accessToken = authorization?.trim().replace('Bearer', '');
+  const isValidToken = /\w_\d_\d/.test(accessToken || '');
 
   if (!accessToken) {
     return {
-      isValidToken: false,
+      isValidToken,
     };
   }
 
   return {
     id: extractIdFromToken(accessToken),
-    isValidToken: true,
+    expiredTimestamp: extractExpiredTimestampFromAccessToken(accessToken),
+    isValidToken,
   };
+};
+
+export const extractIdFromCookie = (
+  req: RestRequest
+): {
+  id?: number;
+  expiredTimestamp?: number;
+  isValidToken?: boolean;
+  isExist: boolean;
+} => {
+  const { refreshToken } = req.cookies;
+  const isValidToken = /\w_refresh_\d_\d/.test(refreshToken);
+
+  if (!refreshToken) {
+    return {
+      isExist: false,
+    };
+  }
+
+  return {
+    id: extractIdFromRefreshToken(refreshToken),
+    expiredTimestamp: extractExpiredTimestampFromRefreshToken(refreshToken),
+    isExist: true,
+    isValidToken,
+  };
+};
+
+export const checkExpiredToken = (expiredTimestamp?: number) => {
+  if (!expiredTimestamp) {
+    return false;
+  }
+
+  expiredTimestamp <= Date.now();
 };
