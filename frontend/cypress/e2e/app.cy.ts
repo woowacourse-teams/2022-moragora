089242b2e5921362c17f85cca3bf2096f0fe186f
kamwoo <rkadndud06@naver.com>
2022-10-18 13:47:06 +0900

    feat: test code 변수 분리

--- frontend/cypress/e2e/app.cy.ts
+++ frontend/cypress/e2e/app.cy.ts
@@ -10,14 +10,14 @@ describe('유저', () => {
       password: 'newuserpw1!',
       passwordConfirm: 'newuserpw1!',
       nickname: '신규회원',
-      code: '123456',
     };
+    const code = '123456';
 
     it('성공 시 참여 중인 모임 목록 페이지로 이동한다.', () => {
       cy.get('a[class*="-RegisterLink"]').should('exist').click();
       cy.get('input[name="email"]').type(newUser.email);
       cy.get('button[class*="-EmailCheckButton"]').click();
-      cy.get('input[name="code"]').type(newUser.code);
+      cy.get('input[name="code"]').type(code);
       cy.get('button[class*="-ConfirmButton"]').click();
       cy.get('input[name="password"]').type(newUser.password);
       cy.get('input[name="passwordConfirm"]').type(newUser.passwordConfirm);
@@ -83,18 +83,18 @@ describe('모임', () => {
       password: 'newuserpw1!',
       passwordConfirm: 'newuserpw1!',
       nickname: '신규회원',
-      code: '123456',
     };
     const newMeeting = {
       name: '새로운 모임',
       member: ['user1', 'user2'],
     };
+    const code = '123456';
 
     it('성공 시 생성된 모임 페이지로 이동한다.', () => {
       cy.get('a[class*="-RegisterLink"]').should('exist').click();
       cy.get('input[name="email"]').type(newUser.email);
       cy.get('button[class*="-EmailCheckButton"]').click();
-      cy.get('input[name="code"]').type(newUser.code);
+      cy.get('input[name="code"]').type(code);
       cy.get('button[class*="-ConfirmButton"]').click();
       cy.get('input[name="password"]').type(newUser.password);
       cy.get('input[name="passwordConfirm"]').type(newUser.passwordConfirm);
