b4df9866e1ce80c701e6cfad2ab712816e052e66
Hongdonggeon <ghd700@naver.com>
2022-10-18 16:54:29 +0900

    github action 테스트를 위한 공백 추가

--- backend/src/main/java/com/woowacourse/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/MoragoraApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class MoragoraApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoragoraApplication.class, args);
     }
