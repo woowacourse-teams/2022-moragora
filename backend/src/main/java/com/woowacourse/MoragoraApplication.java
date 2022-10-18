6e3bb3daf7e0b5fac831eadccb333922dc5e952c
Hongdonggeon <ghd700@naver.com>
2022-10-18 17:05:51 +0900

    style: 공백 추가

--- backend/src/main/java/com/woowacourse/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/MoragoraApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class MoragoraApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MoragoraApplication.class, args);
     }
